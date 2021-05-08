c369c2f15c559c8d2ed7c5ae688a1f212109aee7
Kamil <kamil.jordonov@iaau.edu.kg>
2021-05-08 23:44:14 +0600

    Initial commit

---
+++ src/test/java/org/kamil/schedule/MyScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kamil.schedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyScheduleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
